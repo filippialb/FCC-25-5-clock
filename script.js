12082e45ad7793fab874940996d02976865ef838
Albuquerque <filippialbuquerque@gmail.com>
2023-10-21 09:05:29 -0300

    clock

--- script.js
+++ script.js
@@ -10,40 +10,98 @@ let timeLeft = document.getElementById('time-left');
 let timer;
 let timerStatus = "begin" //begin counting stopped
 let timerLabel = document.getElementById('timer-label');
+let timePaused = "0";
 
 
 startStopButton.addEventListener("click", () => {
-    if (timerStatus === "begin" || timerStatus === "stopped"){
-        
-        //start the timer
-        console.log('start stop button');
-        timerStatus = "counting"
-        timer = setInterval(() => {
-            timeLeft.innerText = decrementTime(timeLeft.innerText);
-        }, 1000);
-        if (timeLeft.innerText === "0:00"){
-            clearInterval(timer);
-        }
-    } else if (timerStatus == "counting"){
-        //stop the timer
-        timerStatus = "stopped";
-        clearInterval(timer);
+    if (timerStatus === "begin" || timerStatus === "counting" || timerStatus === "stopped"){
+        cronometerRun();
+    } else {
+        breakTime();
     }
 })
 
 
-function breakTime() {
-    timerLabel.innerText = "Break"
+
+function cronometerRun(){
+    if (timerStatus === "begin"){
+        
+        //start the timer
+        console.log('start stop button');
+        timerStatus = "counting"
+        
+        if (timePaused === "0") {
+            timeLeft.innerText = sessionLength.innerText + ":00"; 
+            //timeLeft.innerText = "00:05"; //changed for a test
+        } else {
+            timeLeft.innerText = timePaused;
+        }
+        timer = setInterval(() => {
+            timeLeft.innerText = decrementTime(timeLeft.innerText);
+            if (timeLeft.innerText === "0-1:59"){
+                clearInterval(timer);
+                timerLabel.innerText = "Break time";
+                timePaused = "0";
+                timerStatus = "breakBegin"
+                breakTime();
+            }
+        }, 200); //update every 1000 miliseconds
+
+    } else if (timerStatus == "counting"){
+        //stop the timer
+        timerStatus = "stopped";
+        clearInterval(timer);
+
+    } else if (timerStatus == "stopped"){
+        timerStatus = "begin"
+        timePaused = timeLeft.innerText;
+        cronometerRun();
+    }
 }
+
+function breakTime(){
+
+    if (timerStatus === "breakBegin") {
+
+        timerStatus = "breakCounting"
+
+        timeLeft.innerText = breakLength.innerText + ":00";
+        //timeLeft.innerText = "0:05" //changed for a test
+        
+        timer = setInterval(() => {
+            timeLeft.innerText = decrementTime(timeLeft.innerText);
+            if (timeLeft.innerText === "0-1:59"){
+                clearInterval(timer);
+                timerLabel.innerText = "Session";
+                timerStatus = "begin";
+                cronometerRun();
+            }
+        }, 200); //update every 1000 miliseconds
+    } else if (timerStatus === "breakCounting"){
+        timerStatus = "stopped";
+        clearInterval(timer);
+    } else if (timerStatus === "breakStopped"){
+        timerStatus = "breakBegin"
+        timePaused = timeLeft.innerText;
+        cronometerRun();
+    }
+
+}
+
 
 
 resetButton.addEventListener("click", () => {
     clearInterval(timer);
-    //timeLeft.innerText = "25:00";
-    timeLeft.innerText = "00:05";
+    timeLeft.innerText = "25:00"; //changed for a test 
+    sessionLength.innerText = "25";
     breakLength.innerText = "5";
     sessionLength.innerText = "25";
+    timerLabel.innerText = "Session";
     console.log('reset button clicked');
+
+
+    //timeLeft.innerText = "00:05";
+    breakLength.innerText = "5"; // changed for a test 
 })
 
 
@@ -62,6 +120,9 @@ function decrementTime(timeString){
     if (secondDisplay <= 9){
         secondDisplay = "0" + secondDisplay;
     }
+    if (minuteDisplay <= 9){
+        minuteDisplay = "0" + minuteDisplay;
+    }
 
     return minuteDisplay + ":" + secondDisplay; 
 }
@@ -76,7 +137,7 @@ breakIncrementButton.addEventListener("click", () => {
 })
 
 breakDecrementButton.addEventListener("click", () => {
-    if (parseInt(breakLength.innerText) > 0){
+    if (parseInt(breakLength.innerText) > 1){
         breakLength.innerText = parseInt(breakLength.innerText) - 1;
     } else {
         true;
@@ -93,10 +154,14 @@ sessionIncrementButton.addEventListener("click", () => {
 })
 
 sessionDecrementButton.addEventListener("click", () => {
-    if (parseInt(sessionLength.innerText) > 0){
+    if (parseInt(sessionLength.innerText) > 1){
         sessionLength.innerText = parseInt(sessionLength.innerText) - 1;
         timeLeft.innerText = sessionLength.innerText + ":00"
     } else {
         true;
     }
 })
+
+
+
+
